be3f19dc7a7309cf027ff74a481e88c380a0621b
Tim Fischbach <tfischbach@codevise.de>
2017-04-05 11:16:43 +0200

    Generate mask sprite once image is ready

--- app/assets/javascript/pageflow/linkmap_page/editor/models/page_configuration_mixin.js
+++ app/assets/javascript/pageflow/linkmap_page/editor/models/page_configuration_mixin.js
@@ -1,64 +1,84 @@
-pageflow.linkmapPage.pageConfigurationMixin = {
-  initialize: function() {
-    this.listenTo(this, 'change:linkmap_color_map_image_id', function(model, imageFileId) {
-      if (imageFileId) {
-        pageflow.linkmapPage.StoredMaskSprite
-          .findOrCreateForImageFileId(imageFileId,
-                                      this.getImageFileUrl('linkmap_color_map_image_id', {
-                                        styleGroup: 'panorama'
-                                      }))
-          .then(
-            function(masks) {
-              model.set('linkmap_masks', masks);
-            },
-            function(error) {
-              model.unset('linkmap_masks');
-              model.unset('linkmap_color_map_image_id');
-
-              if (error.i18nKey) {
-                alert(I18n.t(error.i18nKey));
-              }
-              else {
-                throw(error);
-              }
-            }
-          );
-      }
-      else {
-        model.unset('linkmap_masks');
-      }
-    });
-  },
-
-  linkmapPageLinks: function() {
-    this._linkmapPageLinks = this._linkmapPageLinks || new pageflow.linkmapPage.PageLinksCollection({
-      areas: this.linkmapAreas()
-    });
-
-    return this._linkmapPageLinks;
-  },
-
-  linkmapAreas: function() {
-    var configuration = this;
-
-    this._linkmapAreas = this._linkmapAreas || create();
-    return this._linkmapAreas;
-
-    function create() {
-      var collection = new pageflow.linkmapPage.AreasCollection(
-        configuration.get('linkmap_areas'),
-        {
-          page: configuration.page
+(function() {
+  pageflow.linkmapPage.pageConfigurationMixin = {
+    initialize: function() {
+      this.listenTo(this, 'change:linkmap_color_map_image_id', function(model, imageFileId) {
+        if (imageFileId) {
+          var imageFile = this.getImageFile('linkmap_color_map_image_id');
+          generateMaskSpriteWhenReady(this, imageFile);
         }
-      );
+        else {
+          resetMaskSprite(this);
+        }
+      });
+    },
 
-      configuration.listenTo(collection, 'add remove change', function() {
-        configuration.set('linkmap_areas', collection.map(function(area) {
-          return _.omit(area.attributes, 'highlighted', 'editing', 'selected');
-        }));
+    linkmapPageLinks: function() {
+      this._linkmapPageLinks = this._linkmapPageLinks || new pageflow.linkmapPage.PageLinksCollection({
+        areas: this.linkmapAreas()
       });
 
-      return collection;
+      return this._linkmapPageLinks;
+    },
+
+    linkmapAreas: function() {
+      var configuration = this;
+
+      this._linkmapAreas = this._linkmapAreas || create();
+      return this._linkmapAreas;
+
+      function create() {
+        var collection = new pageflow.linkmapPage.AreasCollection(
+          configuration.get('linkmap_areas'),
+          {
+            page: configuration.page
+          }
+        );
+
+        configuration.listenTo(collection, 'add remove change', function() {
+          configuration.set('linkmap_areas', collection.map(function(area) {
+            return _.omit(area.attributes, 'highlighted', 'editing', 'selected');
+          }));
+        });
+
+        return collection;
+      }
+    }
+  };
+
+  function generateMaskSpriteWhenReady(configuration, imageFile) {
+    if (imageFile.isReady()) {
+      generateMaskSprite(configuration, imageFile);
+    }
+    else {
+      imageFile.once('change:state', function() {
+        generateMaskSpriteWhenReady(configuration, imageFile);
+      });
     }
   }
-};
+
+  function generateMaskSprite(configuration, imageFile) {
+    pageflow.linkmapPage.StoredMaskSprite
+      .findOrCreateForImageFileId(imageFile.id,
+                                  imageFile.get('panorama_url'))
+      .then(
+        function(masks) {
+          configuration.set('linkmap_masks', masks);
+        },
+        function(error) {
+          configuration.unset('linkmap_masks');
+          configuration.unset('linkmap_color_map_image_id');
+
+          if (error.i18nKey) {
+            alert(I18n.t(error.i18nKey));
+          }
+          else {
+            throw(error);
+          }
+        }
+      );
+  }
+
+  function resetMaskSprite(configuration) {
+    configuration.unset('linkmap_masks');
+  }
+}());
