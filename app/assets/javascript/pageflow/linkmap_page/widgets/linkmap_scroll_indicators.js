edd920f401c2b504842cb6e7acea7222807befcf
Tim Fischbach <tfischbach@codevise.de>
2017-11-10 16:44:23 +0100

    Hide linkmap scroll indicators for pan/zoom

--- app/assets/javascript/pageflow/linkmap_page/widgets/linkmap_scroll_indicators.js
+++ app/assets/javascript/pageflow/linkmap_page/widgets/linkmap_scroll_indicators.js
@@ -6,9 +6,15 @@
       var element = this.element;
       var scroller = this.options.scroller;
 
+      this.arrows = this.element.find('.linkmap-arrow');
+
       scroller.onScroll(toggle);
       scroller.onScrollEnd(toggle);
       toggle();
+
+      if (this.options.disabled) {
+        this.disable();
+      }
 
       function toggle() {
         element.toggleClass('can_scroll_left', Math.ceil(scroller.positionX()) < - margin);
@@ -16,6 +22,14 @@
         element.toggleClass('can_scroll_up', Math.ceil(scroller.positionY()) < - margin);
         element.toggleClass('can_scroll_down', Math.floor(scroller.positionY()) > scroller.maxY() + margin);
       }
+    },
+
+    disable: function() {
+      this.arrows.hide();
+    },
+
+    enable: function() {
+      this.arrows.show();
     }
   });
 }(jQuery));
