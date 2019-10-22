28c3e28cdfdd2838a8e0c17f1615913de87eb895
Tim Fischbach <tfischbach@codevise.de>
2019-10-22 17:57:29 +0200

    File association need not be optional
    Records are restored with correctly rewritten foreign keys.

--- app/models/pageflow/linkmap_page/generated_image_file.rb
+++ app/models/pageflow/linkmap_page/generated_image_file.rb
@@ -5,7 +5,7 @@ module Pageflow
 
       include Pageflow::ReusableFile
 
-      belongs_to :source_image_file, class_name: 'Pageflow::ImageFile', optional: true
+      belongs_to :source_image_file, class_name: 'Pageflow::ImageFile'
 
       GEOMETRY = '1920x1080^'.freeze
 
