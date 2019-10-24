d485d7c7f48fa3de9b1d1296e2f37d7ae1dc049f
Tim Fischbach <tfischbach@codevise.de>
2019-10-24 16:20:42 +0200

    Specify prerequisites for file type export/import tests

--- spec/integration/masked_image_file_type_spec.rb
+++ spec/integration/masked_image_file_type_spec.rb
@@ -6,10 +6,18 @@ module Pageflow
   module LinkmapPage
     Pageflow::Lint.file_type(:masked_image_file,
                              create_file_type: -> { LinkmapPage.masked_image_file_type },
-                             create_file: -> { create(:masked_image_file) })
+                             create_file: -> { create(:masked_image_file) },
+                             create_prerequisite_file_types: -> {
+                               [BuiltInFileType.image, LinkmapPage.color_map_file_type]
+                             },
+                             get_prerequisite_files: ->(file) {
+                               [*file.prerequisite_files, *file.color_map_file.prerequisite_files]
+                             })
 
     Pageflow::Lint.file_type(:color_map_file,
                              create_file_type: -> { LinkmapPage.color_map_file_type },
-                             create_file: -> { create(:color_map_file) })
+                             create_file: -> { create(:color_map_file) },
+                             create_prerequisite_file_types: -> { [BuiltInFileType.image] },
+                             get_prerequisite_files: ->(file) { file.prerequisite_files })
   end
 end
